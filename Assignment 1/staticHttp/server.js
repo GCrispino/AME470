86507478248fac96a3dcff7c79b9ee24f3642302
Gabriel Crispino <Gabriel Crispino>
2016-01-30 12:17:29 -0700

    Server program updated

--- Assignment 1/staticHttp/server.js
+++ Assignment 1/staticHttp/server.js
@@ -1,3 +1,26 @@
+//function that parses the user inputs
+function parseInput(userInput){
+	userInput = decodeURIComponent(userInput);
+	console.log(userInput);
+	var result;
+
+	//has to get line which contains the 'return' keyword and remove it
+	if (userInput.search("return") != -1){
+		console.log("user input: " + userInput);
+		var resVar = userInput.replace(/(.*)return(\s)/,"");
+		userInput = userInput.replace(/return(.*)/g,"");
+		console.log("after");
+		console.log(resVar,userInput);
+		result = eval(resVar);
+	}
+	else
+    	result = eval(userInput);
+
+
+	return result;
+}
+
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -11,19 +34,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/eval", function (req, res) {
-    console.log(req.query);
-    var userInput = decodeURIComponent(req.query.code);
-    var result  = eval(userInput);
-    console.log(userInput,result);
-    // calculate res here
-    //
-     res.send(result.toString()); // send response bodyParser
+    var result = parseInput(req.query.code)
+    res.send(result.toString()); // send response bodyParser
 });
 
-// app.use(methodOverride());
-// app.use(bodyParser());
+app.use(methodOverride());
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
-// app.use(errorHandler());
+app.use(errorHandler());
 
-// console.log("Simple static server listening at http://" + hostname + ":" + port);
+console.log("Simple static server listening at http://" + hostname + ":" + port);
 app.listen(port);
